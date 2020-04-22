a4802acd4b5abbd88d54888542d590045f04d068
NettoZZ <62664647+NettoZZ@users.noreply.github.com>
2020-04-22 16:08:55 -0300

    Add files via upload

---
+++ curso-php/tipos/aritmeticas.php
@@ -0,0 +1,7 @@
+<div class="titulo">Operações Aritmeticas</div>
+
+<?php
+echo 1+1, '<br>';
+var_dump(1+1);
+echo '<br>';
+echo 1 + 2.5, '<br>';
---
+++ curso-php/tipos/int.php
@@ -0,0 +1,12 @@
+<div class="titulo">Tipo Inteiro</div>
+
+<?php
+echo 11;
+echo '<br>';
+
+var_dump(11);
+echo PHP_INT_MAX, '<br>';
+echo PHP_INT_MIN, '<br>';
+echo 011, '<br>'; // Base octal
+echo 0b11, '<br>'; // Base binária
+echo 0x11acf0; // Base hexa decimal
